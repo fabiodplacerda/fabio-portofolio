510545aea435a859c27636101d6d5e94ea9b96e6
fabiodplacerda <lacerda96@gmail.com>
2024-01-16 19:59:10 +0000

    about me section

---
+++ src/About.jsx
@@ -0,0 +1,52 @@
+import SchoolIcon from '@mui/icons-material/School';
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
+import WorkIcon from '@mui/icons-material/Work';
+
+const About = () => {
+  return (
+    <section id="about">
+      <p className="p1">Get to know me</p>
+      <h2>About me</h2>
+      <div className="about-main-container">
+        <img
+          src="profile-pic.png"
+          alt="profile picture"
+          className="about-pic"
+        />
+        <div className="details-main-container">
+          <div className="about-inner-containers">
+            <div className="details-inner-container">
+              <WorkIcon sx={{ fontSize: 40 }} />
+              <h3>Experience</h3>
+              <p>
+                Northcoders - Software Engineer Bootcamp
+                <br />
+              </p>
+            </div>
+            <div className="details-inner-container">
+              <SchoolIcon sx={{ fontSize: 40 }} />
+              <h3>Education</h3>
+              <p>Management & Administration</p>
+            </div>
+          </div>
+          <div className="text-container">
+            <p>
+              I am a Highly Resilient, enthusiastic, and logically driven
+              programmer looking to kick-start my career in Software
+              Development. I am a results-driven professional and decided to
+              pursue a role as a Software engineer because I'm fascinated by the
+              tech industry and what I could potentially do. Recently embarked
+              on a Full-stack Web Development Bootcamp gaining proficiency in
+              Javascript, Test-driven development, PSQL, and React
+            </p>
+          </div>
+        </div>
+      </div>
+      <a href="">
+        <KeyboardDoubleArrowDownIcon sx={{ fontSize: 50 }} className="arrow" />
+      </a>
+    </section>
+  );
+};
+
+export default About;
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './Header';
 import MyProfile from './MyProfile';
+import About from './About';
 
 function App() {
   return (
     <>
       <Header />
       <MyProfile />
+      <About />
     </>
   );
 }
