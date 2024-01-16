b78ffdbc0857752b6da7eac6c0e9bf9822488d7e
fabiodplacerda <lacerda96@gmail.com>
2024-01-16 16:56:43 +0000

    added my profile section to website

---
+++ src/MyProfile.jsx
@@ -0,0 +1,45 @@
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+const MyProfile = () => {
+  return (
+    <section id="profile">
+      <div className="img-container">
+        <img src="profile-pic.png" alt="Fabio Lacerda profile picture" />
+      </div>
+      <div className="profile-text">
+        <p className="p1">Hello Word!</p>
+        <h2>I'm Fabio Lacerda</h2>
+        <p className="p2">Frontend Developer</p>
+        <div className="btn-container">
+          <button
+            className="btn-resume"
+            onClick={() => {
+              window.open('FabioCV-v2.pdf', '_blank');
+            }}
+          >
+            My resume
+          </button>
+          <button
+            className="btn-contact"
+            onClick={() => {
+              location.href = './#contact';
+            }}
+          >
+            Contact Info
+          </button>
+        </div>
+        <div className="socials-container">
+          <a href="https://www.linkedin.com/in/fabiolacerda96/" target="_blank">
+            <LinkedInIcon sx={{ fontSize: 45, color: 'rgb(121, 121, 121)' }} />
+          </a>
+          <a href="https://github.com/fabiodplacerda" target="_blank">
+            <GitHubIcon sx={{ fontSize: 45, color: 'rgb(121, 121, 121)' }} />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MyProfile;
